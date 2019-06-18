4b20fb1b3822efea941b4bb0fe8c1063d369ee7f
Dave Redmin <david.redmin@beta.dhs.gov>
2019-06-18 12:41:50 -0400

    Add some simple molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,7 +10,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+@pytest.mark.parametrize(
+    "directory", [{"path": "/var/pca/pca-gophish-composition", "mode": "0o755"}]
+)
+def test_packages(host, directory):
+    """Test that the appropriate directories were created."""
+    assert host.file(directory["path"]).exists
+    assert host.file(directory["path"]).is_directory
+    assert oct(host.file(directory["path"]).mode) == directory["mode"]
+
+
+@pytest.mark.parametrize("f", ["/var/pca/pca-gophish-composition/docker-compose.yml"])
+def test_command(host, f):
+    """Test that appropriate files exist."""
+    assert host.file(f).exists
+    assert host.file(f).is_file
