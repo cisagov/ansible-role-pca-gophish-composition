db6f39e367bc629e5e4d28de2c9b05d7131298d6
Dave Redmin <david.redmin@beta.dhs.gov>
2019-07-16 23:47:55 -0400

    * Install the pca-gophish-composition package (a.k.a. gophish_init) in order to properly install the gophish_init script * Add a test to verify that the gophish_init package was installed

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -25,3 +25,13 @@ def test_command(host, f):
     """Test that appropriate files exist."""
     assert host.file(f).exists
     assert host.file(f).is_file
+
+
+# Even though the module name is gophish_init (with an underscore) in setup.py
+# (in the pca-gophish-composition repo), as far as pip is concerned, the
+# package name is gophish-init (with a hyphen).
+# https://stackoverflow.com/questions/19097057/pip-e-no-magic-underscore-to-dash-replacement
+@pytest.mark.parametrize("pkg", ["gophish-init"])
+def test_pip_packages(host, pkg):
+    """Test that the pip packages were installed."""
+    assert pkg in host.pip_package.get_packages()
