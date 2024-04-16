fa6857168d56a8fb35782f1e68c49fde04a816fa
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-04-16 16:39:06 -0400

    Correct the name of the imported pip package for Fedora 39
    Right now the version of pip that ships with Fedora 39 _does not_
    insist on normalizing the package name.  I think this may be new
    behavior as of pip version 23.2, which was the first version to
    include the changes in pypa/pip#12044.  (This PR was a fix to address
    pypa/pip#12038.)
    
    See also:
    https://pip.pypa.io/en/stable/news/#v23-2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -44,15 +44,30 @@ def test_packages(host):
         assert host.package(pkg).is_installed
 
 
-# Even though the module name is gophish_init (with an underscore) in setup.py
-# (in the pca-gophish-composition repo), as far as pip is concerned, the
-# package name is gophish-init (with a hyphen).
-# https://stackoverflow.com/questions/19097057/pip-e-no-magic-underscore-to-dash-replacement
-@pytest.mark.parametrize("pkg", ["gophish-init"])
-def test_pip_packages(host, pkg):
+def test_pip_packages(host):
     """Test that the pip packages were installed."""
-    # Note that we are using the version of pip in the Python virtual
-    # environment that has been created.
-    assert pkg in host.pip.get_packages(
-        pip_path="/var/pca/pca-gophish-composition/.venv/bin/pip"
-    )
+    if host.system_info.distribution in ["fedora"] and host.system_info.release in [
+        "39"
+    ]:
+        # Right now the version of pip that ships with Fedora 39 _does
+        # not_ insist on normalizing the package name.  I think this
+        # may be new behavior as of pip version 23.2, which was the
+        # first version to include the changes in pypa/pip#12044.
+        # (This PR was a fix to address pypa/pip#12038.)
+        #
+        # https://pip.pypa.io/en/stable/news/#v23-2
+        pkgs = ["gophish_init"]
+    else:
+        # Even though the module name is gophish_init (with an
+        # underscore) in setup.py (in the pca-gophish-composition
+        # repo), as far as older versions of pip are concerned the
+        # package name is gophish-init (with a hyphen).
+        # https://stackoverflow.com/questions/19097057/pip-e-no-magic-underscore-to-dash-replacement
+        pkgs = ["gophish-init"]
+
+    for pkg in pkgs:
+        # Note that we are using the version of pip in the Python
+        # virtual environment that has been created.
+        assert pkg in host.pip.get_packages(
+            pip_path="/var/pca/pca-gophish-composition/.venv/bin/pip"
+        )
